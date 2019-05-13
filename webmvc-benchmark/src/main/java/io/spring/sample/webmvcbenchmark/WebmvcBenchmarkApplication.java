15c94b23c15bba414afbf0875e5122a6c236f755
Violeta Georgieva <vgeorgieva@pivotal.io>
2019-05-13 13:02:11 +0300

    Set Tomcat's "maxKeepAliveRequests" to "-1"

--- webmvc-benchmark/src/main/java/io/spring/sample/webmvcbenchmark/WebmvcBenchmarkApplication.java
+++ webmvc-benchmark/src/main/java/io/spring/sample/webmvcbenchmark/WebmvcBenchmarkApplication.java
@@ -16,13 +16,29 @@
 
 package io.spring.sample.webmvcbenchmark;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebmvcBenchmarkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebmvcBenchmarkApplication.class, args);
+	}
+
+	@Bean
+	TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+
+			@Override
+			protected void customizeConnector(Connector connector) {
+				connector.setProperty("maxKeepAliveRequests", "-1");
+				super.customizeConnector(connector);
+			}
+		};
+		return factory;
 	}
 }
